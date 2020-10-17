c09ef26c585601aed252bf703969c26b889bce64
hxx <hxx0215@126.com>
2020-10-17 00:00:35 -0400

    [Feature] support all simple message and some event message

--- src/main/scala/zio/wechat/model/package.scala
+++ src/main/scala/zio/wechat/model/package.scala
@@ -3,7 +3,7 @@ package zio.wechat
 import io.circe.{Decoder, Encoder, HCursor, Json}
 import sttp.tapir.{Codec, DecodeResult}
 
-import scala.xml.{Elem, XML}
+import scala.xml.{Elem, MetaData, NamespaceBinding, Node, XML}
 import shapeless.syntax.std.tuple._
 import sttp.tapir.Codec.XmlCodec
 
@@ -38,163 +38,258 @@ package object model {
 
   case class WechatAppConfig(appId: String, appSecret: String, token: String, aesKey: String)
 
-  def baseMessage(root: Elem) = {
-    (
-      (root \ "ToUserName").text,
-      (root \ "FromUserName").text,
-      (root \ "CreateTime").text.toInt,
-      (root \ "MsgId").text.toLong
-    )
+  trait WechatMessage {
+    def toUsername: String
+
+    def fromUsername: String
+
+    def createTime: Int
+
+    def msgType: String
+
+    def msgId: Long
   }
 
-  def baseXML(m: WechatRequestMessage) = {
-    <xml>
-      <ToUserName>
-        {scala.xml.PCData(m.toUsername)}
-      </ToUserName>
-      <FromUserName>
-        {scala.xml.PCData(m.fromUsername)}
-      </FromUserName>
-      <CreateTime>
-        {m.createTime}
-      </CreateTime>
-      <MsgType>
-        {scala.xml.PCData(m.msgType)}
-      </MsgType>
-      <MsgId>
-        {m.msgId}
-      </MsgId>
-    </xml>
+  sealed trait WechatRequestMessage extends WechatMessage
+
+  object WechatRequestMessage {
+    def baseMessage(root: Elem): (String, String, Int, Long) = {
+      (
+        (root \ "ToUserName").text,
+        (root \ "FromUserName").text,
+        (root \ "CreateTime").text.toInt, {
+        val text = (root \ "MsgId").text
+        // Event do not have msg id
+        if (text == "") {
+          0L
+        } else {
+          text.toLong
+        }
+      }
+
+      )
+    }
+
+    def fromString(xmlString: String): DecodeResult[WechatRequestMessage] = {
+      val root = XML.loadString(xmlString)
+      (root \ "MsgType").text match {
+        case "text" => DecodeResult.Value {
+          TextRequestMessage.apply _ tupled baseMessage(root) :+ (root \ "Content").text
+        }
+        case "image" =>
+          DecodeResult.Value(
+            ImageRequestMessage.apply _ tupled baseMessage(root) ++ ((root \ "PicUrl").text, (root \ "MediaId").text)
+          )
+        case "voice" =>
+          DecodeResult.Value(
+            VoiceRequestMessage.apply _ tupled baseMessage(root) ++ ((root \ "Format").text, (root \ "MediaId").text, (root \ "Recognition").headOption.map(_.text))
+          )
+        case "video" =>
+          DecodeResult.Value(
+            VideoRequestMessage.apply _ tupled baseMessage(root) ++ ((root \ "MediaId").text, (root \ "ThumbMediaId").text)
+          )
+        case "shortvideo" =>
+          DecodeResult.Value(
+            ShortVideoRequestMessage.apply _ tupled baseMessage(root) ++ ((root \ "MediaId").text, (root \ "ThumbMediaId").text)
+          )
+        case "location" =>
+          DecodeResult.Value(
+            LocationRequestMessage.apply _ tupled baseMessage(root) ++ ((root \ "Location_X").text.toDouble, (root \ "Location_Y").text.toDouble, (root \ "Scale").text.toDouble, (root \ "Label").text)
+          )
+        case "link" =>
+          DecodeResult.Value(
+            LinkRequestMessage.apply _ tupled baseMessage(root) ++ ((root \ "Title").text, (root \ "Description").text, (root \ "Url").text)
+          )
+        case "event" =>
+          (root \ "Event").text match {
+            case "subscribe" =>
+              DecodeResult.Value(
+                SubscribeEvent.apply _ tupled baseMessage(root)
+              )
+            case "unsubscribe" =>
+              DecodeResult.Value(
+                UnsubscribeEvent.apply _ tupled baseMessage(root)
+              )
+          }
+        case _ => DecodeResult.Missing
+      }
+    }
   }
 
-  sealed trait WechatRequestMessage {
-    val toUsername: String
-    val fromUsername: String
-    val createTime: Int
-    val msgType: String
-    val msgId: Long
-    def toXML: String
+  implicit val wechatRequestMessage: XmlCodec[WechatRequestMessage] = Codec.xml(WechatRequestMessage.fromString) {
+    // no used cause you will not response a request message, so no need to encode it to string
+    _ => ""
   }
-
-  implicit val wechatRequestMessage: XmlCodec[WechatRequestMessage] = Codec.xml(str =>{
-    val root =XML.loadString(str)
-    val messageType = (root \ "MsgType").text
-    (messageType match {
-      case "text" => TextRequestMessage.fromString(str)
-      case "image" => ImageRequestMessage.fromString(str)
-      case "voice" => VoiceRequestMessage.fromString(str)
-      case _ => DecodeResult.Missing
-    })
-  })(_.toXML)
 
   case class TextRequestMessage(toUsername: String, fromUsername: String, createTime: Int, msgId: Long, content: String) extends WechatRequestMessage {
     override val msgType: String = "text"
 
-    override def toXML: String = {
-      val base = baseXML(this)
-      val xmlEl = base.copy(child = base.child :+ {
-        <Content>
-          {scala.xml.PCData(content)}
-        </Content>
-      })
-      xmlEl.toString()
-    }
-  }
-
-  object TextRequestMessage{
-    def fromString(str: String): DecodeResult.Value[TextRequestMessage] = {
-      val root = XML.loadString(str)
-      val args = baseMessage(root) :+ (root \ "Content").text
-      DecodeResult.Value(
-        (TextRequestMessage.apply _) tupled args
-      )
-    }
+    def extraFieldToXMLs = Seq(
+      <Content>
+        {scala.xml.PCData(content)}
+      </Content>
+    )
   }
 
   case class ImageRequestMessage(toUsername: String, fromUsername: String, createTime: Int, msgId: Long, picUrl: String, mediaId: String) extends WechatRequestMessage {
     override val msgType: String = "image"
 
-    override def toXML: String = {
-      val base = baseXML(this)
-      val xmlEl = base.copy(child = base.child ++ Seq(
-        <PicUrl>
-          {scala.xml.PCData(picUrl)}
-        </PicUrl>,
-        <MediaId>
-          {scala.xml.PCData(mediaId)}
-        </MediaId>
-      ))
-      xmlEl.toString()
-    }
-  }
-
-  object ImageRequestMessage{
-    def fromString(str: String): DecodeResult.Value[ImageRequestMessage] = {
-      val root = XML.loadString(str)
-      val args = baseMessage(root) ++ ((root \ "PicUrl").text, (root \ "MediaId").text)
-      DecodeResult.Value(
-        (ImageRequestMessage.apply _) tupled args
-      )
-    }
+    def extraFieldToXMLs = Seq(
+      <PicUrl>
+        {scala.xml.PCData(picUrl)}
+      </PicUrl>,
+      <MediaId>
+        {scala.xml.PCData(mediaId)}
+      </MediaId>
+    )
   }
 
   case class VoiceRequestMessage(toUsername: String, fromUsername: String, createTime: Int, msgId: Long, format: String, mediaId: String, recognition: Option[String] = None) extends WechatRequestMessage {
     override val msgType: String = "voice"
 
-    override def toXML: String = {
-      val base = baseXML(this)
-      val xmlEl = base.copy(child = base.child ++ Seq(
-        <MediaId>
-          {scala.xml.PCData(mediaId)}
-        </MediaId>,
-        <Format>
-          {scala.xml.PCData(format)}
-        </Format>
-      ) ++ {
-        recognition.map(r => <Recognition>{scala.xml.PCData(r)}</Recognition>).toSeq
-      })
-      xmlEl.toString()
-    }
-  }
-  object VoiceRequestMessage{
-    def fromString(str: String): DecodeResult.Value[VoiceRequestMessage] = {
-      val root = XML.loadString(str)
-      val args = baseMessage(root) ++ ((root \ "Format").text, (root \ "MediaId").text, (root \ "Recognition").headOption.map(_.text))
-      DecodeResult.Value(
-        (VoiceRequestMessage.apply _) tupled args
-      )
+    def extraFieldToXMLs: Seq[Elem] = Seq(
+      <MediaId>
+        {scala.xml.PCData(mediaId)}
+      </MediaId>,
+      <Format>
+        {scala.xml.PCData(format)}
+      </Format>
+    ) ++ {
+      recognition.map(r => <Recognition>
+        {scala.xml.PCData(r)}
+      </Recognition>).toSeq
     }
   }
 
+  case class VideoRequestMessage(toUsername: String, fromUsername: String, createTime: Int, msgId: Long, mediaId: String, thumbMediaId: String) extends WechatRequestMessage {
+    override val msgType: String = "video"
 
-    case class VideoRequestMessage(toUsername: String, fromUsername: String, createTime: Int, msgId: Long, mediaId: String, thumbMediaId: String) extends WechatRequestMessage {
-      override val msgType: String = "video"
+    def extraFieldToXMLs = Seq(
+      <MediaId>
+        {scala.xml.PCData(mediaId)}
+      </MediaId>,
+      <ThumbMediaId>
+        {scala.xml.PCData(thumbMediaId)}
+      </ThumbMediaId>
+    )
+  }
 
-      override def toXML: String = {
-        val base = baseXML(this)
-        val xmlEL = base.copy(child = base.child ++ Seq(
-          <MediaId>{scala.xml.PCData(mediaId)}</MediaId>,
-          <ThumbMediaId>{scala.xml.PCData(thumbMediaId)}</ThumbMediaId>
-        ))
-        xmlEL.toString()
+  case class ShortVideoRequestMessage(toUsername: String, fromUsername: String, createTime: Int, msgId: Long, mediaId: String, thumbMediaId: String) extends WechatRequestMessage {
+    override val msgType: String = "shortvideo"
+
+    def extraFieldToXMLs = Seq(
+      <MediaId>
+        {scala.xml.PCData(mediaId)}
+      </MediaId>,
+      <ThumbMediaId>
+        {scala.xml.PCData(thumbMediaId)}
+      </ThumbMediaId>
+    )
+  }
+
+  case class LocationRequestMessage(toUsername: String, fromUsername: String, createTime: Int, msgId: Long, locationX: Double, locationY: Double, scale: Double, label: String) extends WechatRequestMessage {
+    override val msgType: String = "location"
+
+    def extraFieldToXMLs = Seq(
+      <Location_X>
+        {locationX}
+      </Location_X>,
+      <Location_Y>
+        {locationY}
+      </Location_Y>,
+      <Scale>
+        {scale}
+      </Scale>,
+      <Label>
+        {scala.xml.PCData(label)}
+      </Label>
+    )
+  }
 
 
+  case class LinkRequestMessage(toUsername: String, fromUsername: String, createTime: Int, msgId: Long, title: String, description: String, url: String) extends WechatRequestMessage {
+    override val msgType: String = "link"
+
+    def extraFieldToXMLs = Seq(
+      <Title>
+        {scala.xml.PCData(title)}
+      </Title>,
+      <Description>
+        {scala.xml.PCData(description)}
+      </Description>,
+      <Url>
+        {scala.xml.PCData(url)}
+      </Url>
+    )
+  }
+
+  trait WechatEvent {
+    def event: String
+
+    def msgType: String = "event"
+  }
+
+  case class SubscribeEvent(toUsername: String, fromUsername: String, createTime: Int, msgId: Long) extends WechatRequestMessage with WechatEvent {
+    override def event: String = "subscribe"
+  }
+
+  case class UnsubscribeEvent(toUsername: String, fromUsername: String, createTime: Int, msgId: Long) extends WechatRequestMessage with WechatEvent {
+    override def event: String = "unsubscribe"
+  }
+
+  sealed trait WechatResponseMessage extends WechatMessage {
+
+    def toXML: Elem = {
+      val base = {
+        <xml>
+          <ToUserName>
+            {scala.xml.PCData(toUsername)}
+          </ToUserName>
+          <FromUserName>
+            {scala.xml.PCData(fromUsername)}
+          </FromUserName>
+          <CreateTime>
+            {createTime}
+          </CreateTime>
+          <MsgType>
+            {scala.xml.PCData(msgType)}
+          </MsgType>
+          <MsgId>
+            {msgId}
+          </MsgId>
+        </xml>
       }
+      base.copy(child = base.child ++ extraFieldToXMLs)
     }
 
-  //  case class ShortVideoMessage(toUsername: String, fromUsername: String, createTime: Int, msgId: Long, mediaId: String, thumbMediaId: String) extends WechatMessage {
-  //    override val msgType: String = "shortvideo"
-  //  }
-  //
-  //  case class LocationMessage(toUsername: String, fromUsername: String, createTime: Int, msgId: Long, locationX: Double, locationY: Double, scale: Double, label: String) extends WechatMessage {
-  //    override val msgType: String = "location"
-  //  }
-  //
-  //  case class LinkMessage(toUsername: String, fromUsername: String, createTime: Int, msgId: Long, title: String, description: String, url: String) extends WechatMessage {
-  //    override val msgType: String = "link"
-  //  }
+    def extraFieldToXMLs: Seq[Elem]
+  }
 
-  trait WechatResponseMessage
-  implicit val responseCodec: Codec.XmlCodec[WechatResponseMessage] = Codec.xml(str => ???)(m => ???)
+  case class TextResponseMessage(toUsername: String, fromUsername: String, createTime: Int, msgId: Long, content: String) extends WechatResponseMessage {
+    override def extraFieldToXMLs = Seq(
+      <Content>
+        {scala.xml.PCData(content)}
+      </Content>
+    )
+
+    override def msgType: String = "text"
+  }
+
+  implicit val responseCodec: Codec.XmlCodec[WechatResponseMessage] = Codec.xml(_ => DecodeResult.Missing)(_.toXML.toString())
+
+  case class EmptyResponseMessage(toUsername: String, fromUsername: String, createTime: Int, msgId: Long) extends WechatResponseMessage {
+    override def extraFieldToXMLs: Seq[Elem] = Seq()
+
+    override def toXML: Elem = {
+      val empty = <xml/>
+      class EmptyElm() extends Elem(empty.prefix, empty.label, empty.attributes, empty.scope, empty.minimizeEmpty, empty.child: _*) {
+        override def toString(): String = "success"
+      }
+      new EmptyElm()
+    }
+
+    override def msgType: String = ""
+  }
+
 
 }
--- src/test/scala/zio/wechat/endpoints/EndpointServerTest.scala
+++ src/test/scala/zio/wechat/endpoints/EndpointServerTest.scala
@@ -8,7 +8,7 @@ import zio.wechat.{configLayer, model}
 import zio.wechat.server.wechatRequestValidate
 import zio.Runtime
 import zio.internal.Executor
-import zio.wechat.model.{ImageRequestMessage, TextRequestMessage, VoiceRequestMessage, WechatRequestMessage, WechatResponseMessage, wechatRequestMessage}
+import zio.wechat.model.{EmptyResponseMessage, ImageRequestMessage, LinkRequestMessage, LocationRequestMessage, ShortVideoRequestMessage, SubscribeEvent, TextRequestMessage, TextResponseMessage, UnsubscribeEvent, VideoRequestMessage, VoiceRequestMessage, WechatRequestMessage, WechatResponseMessage, wechatRequestMessage}
 
 import scala.concurrent.{ExecutionContextExecutor, Future}
 
@@ -17,12 +17,6 @@ object EndpointServerTest extends App {
   implicit val actorSystem: ActorSystem = ActorSystem()
   implicit val dispatcher: ExecutionContextExecutor = actorSystem.getDispatcher
   val runtime = Runtime.default.withExecutor(Executor.fromExecutionContext(1000)(dispatcher))
-  //  val hookRoute: Route = webHook("web" / "hook").toRoute {
-  //    case (_, signature, timestamp, nonce, echostr,body) =>
-  //      println(s"signature: $signature,timestamp: $timestamp, nonce: $nonce, echostr: $echostr")
-  //      println(s"body:$body")
-  //      runtime.unsafeRunToFuture(wechatRequestValidate(signature, timestamp, nonce, echostr).provideLayer(configLayer())).map(Right(_))
-  //  }
 
   import sttp.tapir._
 
@@ -32,21 +26,32 @@ object EndpointServerTest extends App {
       println(s"signature: $signature,timestamp: $timestamp, nonce: $nonce, echostr: $echostr")
       runtime.unsafeRunToFuture(wechatRequestValidate(signature, timestamp, nonce, echostr).provideLayer(configLayer())).map(Right(_))
   }
-  val textRoute: Route = hook.wechatMessageEndpoint().toRoute {
+  val textRoute: Route = hook.wechatMessageEndpoint[WechatRequestMessage, WechatResponseMessage]().toRoute {
     case (_, signature, timestamp, nonce, body) =>
       println(s"signature: $signature,timestamp: $timestamp, nonce: $nonce, body: $body")
-      val response =
+      val response: WechatResponseMessage =
         body match {
           case TextRequestMessage(toUsername, fromUsername, createTime, msgId, content) =>
-            TextRequestMessage(fromUsername, toUsername, (System.currentTimeMillis() / 1000).toInt, msgId, "收到文字")
+            TextResponseMessage(fromUsername, toUsername, (System.currentTimeMillis() / 1000).toInt, msgId, "收到文字")
           case ImageRequestMessage(toUsername, fromUsername, createTime, msgId, picUrl, mediaId) =>
-            TextRequestMessage(fromUsername, toUsername, (System.currentTimeMillis() / 1000).toInt, msgId, "图片")
+            TextResponseMessage(fromUsername, toUsername, (System.currentTimeMillis() / 1000).toInt, msgId, "图片")
           case VoiceRequestMessage(toUsername, fromUsername, createTime, msgId, format, mediaId, recognition) =>
-            TextRequestMessage(fromUsername, toUsername, (System.currentTimeMillis() / 1000).toInt, msgId, "声音")
-          case _ =>
-            WechatRequestMessage
+            TextResponseMessage(fromUsername, toUsername, (System.currentTimeMillis() / 1000).toInt, msgId, "声音")
+          case VideoRequestMessage(toUsername, fromUsername, createTime, msgId, mediaId, thumbMediaId) =>
+            TextResponseMessage(fromUsername, toUsername, (System.currentTimeMillis() / 1000).toInt, msgId, "视频")
+          case ShortVideoRequestMessage(toUsername, fromUsername, createTime, msgId, mediaId, thumbMediaId) =>
+            TextResponseMessage(fromUsername, toUsername, (System.currentTimeMillis() / 1000).toInt, msgId, "小视频")
+          case LocationRequestMessage(toUsername, fromUsername, createTime, msgId, locationX, locationY, scale, label) =>
+            TextResponseMessage(fromUsername, toUsername, (System.currentTimeMillis() / 1000).toInt, msgId, "位置")
+          case LinkRequestMessage(toUsername, fromUsername, createTime, msgId, title, description, url) =>
+            TextResponseMessage(fromUsername, toUsername, (System.currentTimeMillis() / 1000).toInt, msgId, "链接")
+          case SubscribeEvent(toUsername, fromUsername, createTime, msgId) =>
+            TextResponseMessage(fromUsername, toUsername, (System.currentTimeMillis() / 1000).toInt, msgId, "欢迎订阅")
+          case UnsubscribeEvent(toUsername, fromUsername, createTime, msgId) =>
+            EmptyResponseMessage(fromUsername, toUsername, (System.currentTimeMillis() / 1000).toInt, msgId)
         }
       Future.successful(Right(response))
+  }
 
   import akka.http.scaladsl.server.Directives._
 
